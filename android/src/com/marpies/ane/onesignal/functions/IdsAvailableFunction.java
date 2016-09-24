726f3c538b8630ae69e5b99f4e8d4ef0ae18e9fb
Marcel Piešťanský <marcel@piestansky.cz>
2016-09-24 12:36:15 +0200

    ADDED Android 'idsAvailable' implementation

---
+++ android/src/com/marpies/ane/onesignal/functions/IdsAvailableFunction.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 Marcel Piestansky (http://marpies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.marpies.ane.onesignal.functions;
+
+import com.adobe.fre.FREContext;
+import com.adobe.fre.FREObject;
+import com.marpies.ane.onesignal.utils.AIR;
+import com.onesignal.OneSignal;
+
+public class IdsAvailableFunction extends BaseFunction {
+
+	@Override
+	public FREObject call( FREContext context, FREObject[] args ) {
+		super.call( context, args );
+
+		/* OneSignal.idsAvailable must be called after OneSignal.init().
+		 * The callback is registered in AS3 so it will be called after
+		 * the SDK is initialized, no point calling OneSignal.idsAvailable here */
+		if( AIR.getOneSignalListener() == null ) return null; // SDK not initialized yet
+
+		AIR.log( "OneSignal::idsAvailable" );
+		/* Call it again with the same listener */
+		OneSignal.idsAvailable( AIR.getOneSignalListener() );
+
+		return null;
+	}
+
+}
+
